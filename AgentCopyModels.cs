f0cd0bfeb3aa765e85d8e5fc70b7d781d65e1042
Luis Pagoto <105325557+PagotoLuis@users.noreply.github.com>
2025-08-14 09:38:29 -0300

    Add files via upload

---
+++ AgentCopyModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AgentCopyModels
+{
+    internal static class AgentCopyModels
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ServiceCopyModels()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
